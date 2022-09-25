330715395db1b8f7c885b7353055bb9e8c75e2a3
vito <vitotu@qq.com>
2022-09-25 21:15:23 +0800

    增加已专注时长&&修改基本布局为flex

--- miniprogram/config/timeFormat.ts
+++ miniprogram/config/timeFormat.ts
@@ -1,4 +1,8 @@
-const zhYMD = 'YYYY年MM月DD日'
+enum Week {
+  '一','二','三','四','五','六','日'
+}
+// TODO add weeks
+const zhYMD = `YYYY年M月D日`
 const zhTime = 'HH:mm:ss.SS'
 
 export default {
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -4,18 +4,39 @@ import moment from 'moment';
 import formatConfig  from '../../config/index'
 const app = getApp<IAppOption>()
 
+interface CustomObject {
+  [key:string]: any;
+}
+
 Page({
+  options:{
+    pureDataPattern: /^_/
+  },
   data: {
     timeClock: moment().format('HH:mm:ss.SS'),
     date: moment().format('YYYY-MM-DD'),
     menuHide:true,
+    countTime: moment('00:00').format('mm:ss'),
+    _startTime: new Date().getTime(),
   },
   // 事件处理函数
   onLoad() {
-    setInterval(() =>{
+    if(!this.timer){
+      this.startTimer();
+    }
+  },
+  onShow(){
+    if(!this.timer){
+      this.startTimer();
+    }
+  },
+  startTimer(){
+    this.setData({_startTime:new Date().getTime()})
+    this.timer = setInterval(() =>{
       this.setData({
         timeClock: moment().format(formatConfig.timeFormat.zhTime),
         date: moment().format(formatConfig.timeFormat.zhYMD),
+        countTime: moment.utc(new Date().getTime() - this.data._startTime).format('H:m:ss')
       })
     }, 63)
   },
@@ -24,7 +45,7 @@ Page({
       menuHide: false 
     })
   },
-  onShareAppMessage(options){
+  onShareAppMessage(options:any){
     console.log(options);
     return {
       title:"全屏专注时钟",
@@ -35,5 +56,22 @@ Page({
     return {
       title:'全屏专注时钟'
     }
+  },
+  onHide(){
+    wx.showToast({
+      title:"退出将重新计时",
+      duration: 2000,
+      icon: 'none'
+    })
+    if(this.timer) {
+      clearInterval(this.timer);
+      this.timer = null;
+    };
+  },
+  onUnload(){
+    if(this.timer) {
+      clearInterval(this.timer);
+      this.timer = null;
+    };
   }
-})
+} as CustomObject)
